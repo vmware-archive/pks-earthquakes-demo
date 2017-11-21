3f077701341d599dd0b091fcd19d520a1d44470e
cpage-pivotal <cpage@pivotal.io>
2017-11-21 15:13:10 -0600

    bugfix

--- ci/run-register-apps.sh
+++ ci/run-register-apps.sh
@@ -4,7 +4,7 @@
 
 set -e -x
 
-pushd scdf-demo/register-apps
+pushd pks-earthquakes-demo/register-apps
   ./mvnw package
   java -jar target/register-apps-0.0.1-SNAPSHOT.jar -DCF_API=$CF_API -DCF_USER=$CF_USER -DCF_PASSWORD=$CF_PASSWORD -DCF_ORG=$CF_ORG -DCF_SPACE=$CF_SPACE
 popd
